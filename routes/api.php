4c8f382b29ad4db44e5e4feabc243bfc02de19df
Sebastian Roldan <sroldan@herfox.com>
2022-01-25 21:03:21 -0500

    Create API for Python

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,10 +20,25 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::resource('doc_types', App\Http\Controllers\API\DocTypeAPIController::class);
+// Route::resource('doc_types', App\Http\Controllers\API\DocTypeAPIController::class);
 
 
-Route::resource('documentations', App\Http\Controllers\API\DocumentationAPIController::class);
+// Route::resource('documentations', App\Http\Controllers\API\DocumentationAPIController::class);
 
 
-Route::resource('content_docs', App\Http\Controllers\API\ContentDocAPIController::class);
+// Route::resource('content_docs', App\Http\Controllers\API\ContentDocAPIController::class);
+
+Route::prefix('v1')->group(function () {
+    Route::get('consult/{nickname}/{platform}', function ($nickname, $platform) {
+        switch ($platform) {
+            case 'str':
+                $url = 'https://stripchat.com/api/front/v2/models/username/'.$nickname.'/cam';
+                break;
+            case 'cht':
+                $url = 'https://chaturbate.com/api/chatvideocontext/'.$nickname;
+                break;
+        }
+        $response = Http::get($url);
+        return response()->json($response->json());
+    });
+});
